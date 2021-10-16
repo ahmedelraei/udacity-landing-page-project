a5e4df511b514c64d70a2b18901f09748af99cd6
Ahmed Hatem <ahmedelrai18@gmail.com>
2021-10-16 23:59:55 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,38 +1,18 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
 const sections = Array.from(document.querySelectorAll('section'));
-const menu = document.getElementById('navbar__list');
 
+window.onload = _=> {
+    sections.map(section => {
+        let navItem = document.createElement('li');
+        navItem.innerHTML = `<a class="menu__link" href="#${section.getAttribute('id')}">${section.getAttribute('data-nav')}</a>`
+        document.getElementById('navbar__list').appendChild(navItem);
+    })
+}
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-const toggleActive = () => {
+document.addEventListener('scroll', _=> {
     let sectionInPositionId = location.hash.slice(1);
     let sectionInPosition = document.getElementById(sectionInPositionId);
 
-    for (section of sections){
+    sections.map(section => {
         if (section.id === sectionInPositionId){
             if (!sectionInPosition.classList.contains('your-active-class')){
                 sectionInPosition.classList.add('your-active-class')
@@ -41,30 +21,5 @@ const toggleActive = () => {
         } else{
             section.classList.remove('your-active-class')
         }
-    }
-}
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-const buildListItem = () => {
-    for (section of sections){
-        let sectionName = section.getAttribute('data-nav');
-        let sectionLink = section.getAttribute('id');
-        let listItem = document.createElement('li');
-
-        listItem.innerHTML = `<a class="menu__link" href="#${sectionLink}">${sectionName}</a>`;
-
-        menu.appendChild(listItem);
-    }
-}
-
-
-// Add class 'active' to section when near top of viewport
-document.addEventListener('scroll', toggleActive);
-
-
-buildListItem();
+    })
+});
