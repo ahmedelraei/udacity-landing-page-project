a90e53f4185f07afa918997ac7957d9135a0d7f5
Ahmed Hatem <ahmedelrai18@gmail.com>
2021-10-18 01:51:27 +0200

    scroll

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
 const sections = Array.from(document.querySelectorAll('section'));
+const navBarList = document.getElementById('navbar__list');
 
-window.onload = _=> {
+
+document.addEventListener('DOMContentLoaded', _=> {
     sections.map(section => {
         let navItem = document.createElement('li');
         navItem.innerHTML = `<a class="menu__link" href="#${section.getAttribute('id')}">${section.getAttribute('data-nav')}</a>`
-        document.getElementById('navbar__list').appendChild(navItem);
+        navBarList.appendChild(navItem);
+        navItem.addEventListener('click', e => {
+            e.preventDefault();
+            section.scrollIntoView({behavior: "smooth"});
+        })
     })
-}
+});
+
 
 document.addEventListener('scroll', _=> {
     let sectionInPositionId = location.hash.slice(1);
     let sectionInPosition = document.getElementById(sectionInPositionId);
 
     sections.map(section => {
-        if (section.id === sectionInPositionId){
-            if (!sectionInPosition.classList.contains('your-active-class')){
-                sectionInPosition.classList.add('your-active-class')
-            }
-
-        } else{
+        if (section.id === sectionInPositionId && !sectionInPosition.classList.contains('your-active-class')){
+            sectionInPosition.classList.add('your-active-class')
+        } 
+        else{
             section.classList.remove('your-active-class')
         }
     })
