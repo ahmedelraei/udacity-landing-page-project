165031ea6f650fad854b8ce79d12d6904ea328a6
Ahmed Hatem <ahmedelrai18@gmail.com>
2021-10-19 18:27:28 +0200

    active classes

--- js/app.js
+++ js/app.js
@@ -14,17 +14,31 @@ document.addEventListener('DOMContentLoaded', _=> {
     })
 });
 
+const inViewPort = (section) => {
+    const elementPlace = Math.floor(section.getBoundingClientRect().top);
+    return elementPlace < 250 && elementPlace >= -250;
+}
+const addActiveClass = (section) => {
+    if(inViewPort(section)){
+        section.classList.add('your-active-class');
+        section.style.cssText = "box-shadow:20px 20px 50px 15px #000000;border:2px solid #000;border-radius:2px";
+        const sectionId= section.id.slice(7,8) -1;
+        navBarList.childNodes[sectionId].style.cssText="background-color:#000000;";
+        navBarList.childNodes[sectionId].firstChild.style.cssText="color: #ffffff;";
+    };
+};
+const removeActiveClass = (section) => {
+    section.classList.remove('your-active-class');
+    section.style.cssText = "box-shadow:none;border:none;border-radius:none";
+    const sectionId= section.id.slice(7,8) -1;
+    navBarList.childNodes[sectionId].style.cssText="background-color:#ffffff;";
+    navBarList.childNodes[sectionId].firstChild.style.cssText="color: #000000;";
+
+};
 
 document.addEventListener('scroll', _=> {
-    let sectionInPositionId = location.hash.slice(1);
-    let sectionInPosition = document.getElementById(sectionInPositionId);
-
     sections.map(section => {
-        if (section.id === sectionInPositionId && !sectionInPosition.classList.contains('your-active-class')){
-            sectionInPosition.classList.add('your-active-class')
-        } 
-        else{
-            section.classList.remove('your-active-class')
-        }
+        removeActiveClass(section);
+        addActiveClass(section);
     })
 });
